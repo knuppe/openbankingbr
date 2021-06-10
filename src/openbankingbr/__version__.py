75910882d75933cd2c9d62f1833b57bc9c9450d1
Knuppe <gustavoknuppe@gmail.com>
2021-06-10 15:45:09 -0300

    Versao 0.1.2

--- src/openbankingbr/__version__.py
+++ src/openbankingbr/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'openbankingbr'
 __description__ = 'Python 3 package to extract public data for Open Banking Brasil.'
 __url__ = 'https://github.com/knuppe/openbankingbr'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Gustavo J Knuppe'
 __author_email__ = 'gustavoknuppe@gmail.com'
 __license__ = 'MIT'
